b2946eb6654a6ddaca81f7e129f941e3925647bc
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-08-30 09:05:25 +0300

    Solve Pointers Section Challenge

--- pointersAndReferences/exersiceThree/main.cpp
+++ pointersAndReferences/exersiceThree/main.cpp
@@ -3,7 +3,7 @@ void reverse_array(int *arr, int size)
     int *start = arr;
     // [1,2,3,4]
     int *end = arr + size - 1;
-    while (start, end)
+    while (start < end)
     {
         // swap elements pointed to by start and end pointers
         int temp = *start;
@@ -15,3 +15,4 @@ void reverse_array(int *arr, int size)
         end--;
     }
 }
+
