47336710a01621561c9833973e3f1929a459931d
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-08-31 11:43:42 +0300

    Accesing class members

--- OOP/ClassAndObjects/DeclareClass/main.cpp
+++ OOP/ClassAndObjects/DeclareClass/main.cpp
@@ -6,33 +6,53 @@ using namespace std;
 
 class Player
 {
-    // attributes
-    string name{"Player"};
-    int health{100};
-    int xp{3};
+    public:
+        // attributes
+        string name{"Player"};
+        int health{100};
+        int xp{3};
 
-    // methods
-    void talk(string);
-    bool is_dead();
+        // methods
+        void talk(string talk_to_say){
+            cout << name << "says" << talk_to_say << endl;
+        };
+        bool is_dead();
 };
 
 class Account
 {
-    // attributes
-    string name{"Account"};
-    double balance{0.0};
+    public:
+        // attributes
+        string name{"Account"};
+        double balance{0.0};
 
-    // methods
-    bool deposit(double);
-    bool withdraw(double);
+        // methods
+        bool deposit(double bal){balance += bal; cout << "In deposit" << endl;};
+        bool withdraw(double bal){balance -= bal; cout << "In deposit" << endl;};
 };
 
 int main()
 {
     Account frank_account;
+    frank_account.name = "Frank account";
+    frank_account.balance = 5000;
+
+    frank_account.deposit(2000);
+    frank_account.withdraw(1000);
+
     Account jim_account;
 
     Player frank;
+    frank.name = "Frank";
+    frank.health = 35;
+    frank.xp = 12;
+    frank.talk("Hello there");
+
+    Player *enemy = new Player;
+    (*enemy).name = "Enemy";
+    enemy->xp = 15;
+    enemy->talk("i will destroy you");
+
     Player hero;
     Player players[]{frank, hero};
 
