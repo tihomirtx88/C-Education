ad00844de1d591b7820a902fff87aa97600d03c0
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-09-12 08:00:46 +0300

    Creating two classes: Parent Accoutn and derived Savings_Account

--- OperatorOverloading/Chellange/Mystring.h
+++ OperatorOverloading/Chellange/Mystring.h
@@ -41,14 +41,19 @@ public:
         // Overloaded operator member methods 
     Mystring operator-() const;                                         // make lowercase
     Mystring operator+(const Mystring &rhs) const;         // concatenate
+
     bool operator==(const Mystring &rhs) const;             // equals
     bool operator!=(const Mystring &rhs) const;              //not equals
     bool operator<(const Mystring &rhs) const;               //less than
     bool operator>(const Mystring &rhs) const;               //greater than
+
     Mystring &operator+=(const Mystring &rhs);            // s1 += s2;  concat and assign
+
     Mystring operator*(int n) const;                                // s1 = s2 * 3;  repeat s2 n times
+
     Mystring &operator*=(int n);                                    // s1 *= 3;   s1 = s1 * 3;    
     Mystring &operator++();                                          // pre-increment    ++s1;
+    
     Mystring operator++(int);                                         // post-increment   s1++;
 };
 
